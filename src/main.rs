6cb37dc207987a3fe724b6c9a7d62a586029d6dc
himkt <himkt@klis.tsukuba.ac.jp>
2024-05-18 14:47:08 +0900

    feat: update bumpversion.toml

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ pub struct Version {
 }
 
 impl Version {
-    pub fn to_str(&self) -> String {
+    pub fn as_string(&self) -> String {
         format!("{}.{}.{}", self.major, self.minor, self.patch)
     }
     pub fn bump(self, action: Action) -> Version {
@@ -50,26 +50,37 @@ impl Version {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opt = Cli::parse();
-    println!("{:?}", opt);
 
     let content = fs::read_to_string("bumpversion.toml")?;
     let config = content.parse::<Value>()?;
     let bumpversion: &Value = config.get("bumpversion").unwrap();
     let version = bumpversion.get("version").unwrap().to_string();
-    println!("{:?}", bumpversion);
-    println!("{:?}", version);
 
     let pattern = Regex::new(r"(\d+)\.(\d+)\.(\d+)")?;
     let result = pattern.captures(&version).unwrap();
-    println!("{:?}", result);
 
     let version_obj = Version {
         major: result.get(1).unwrap().as_str().parse::<usize>()?,
         minor: result.get(2).unwrap().as_str().parse::<usize>()?,
         patch: result.get(3).unwrap().as_str().parse::<usize>()?,
     };
-    println!("{:?}", version_obj);
     let version_obj = version_obj.bump(opt.action);
-    println!("{:?}", version_obj);
+
+    let mut table = config.clone();
+    table
+        .as_table_mut()
+        .unwrap()
+        .get_mut("bumpversion")
+        .unwrap()
+        .as_table_mut()
+        .unwrap()
+        .insert(
+            "version".to_string(),
+            Value::String(version_obj.as_string()),
+        );
+
+    let new_content = toml::to_string(&table)?;
+    fs::write("bumpversion.toml", new_content)?;
+
     Ok(())
 }
